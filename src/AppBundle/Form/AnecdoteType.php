bbc188a25f97a346e4229ec64e48916a537cbdac
Quentin Delance <quentin.delance@gmail.com>
2016-03-22 13:35:39 +0100

    More i18n work

--- src/AppBundle/Form/AnecdoteType.php
+++ src/AppBundle/Form/AnecdoteType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,19 +18,39 @@ class AnecdoteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('description')
-            ->add(
-                'category',
-                EntityType::class,
-                array(
-                    'class' => 'AppBundle:Category',
-                    'choice_label' => 'name',
-                    'multiple' => false,
-                )
+          ->add(
+            'description',
+            null,
+            array(
+              'attr' => array('autofocus' => true),
+              'label' => 'label.description',
             )
-            ->add('nickname')
-            ->add('email')
-            ->add('save', SubmitType::class);
+          )
+          ->add(
+            'category',
+            EntityType::class,
+            array(
+              'label' => 'label.category',
+              'class' => 'AppBundle:Category',
+              'choice_label' => 'name',
+              'multiple' => false,
+            )
+          )
+          ->add(
+            'nickname',
+            TextType::class,
+            array('label' => 'label.nickname')
+          )
+          ->add(
+            'email',
+            TextType::class,
+            array('label' => 'label.email')
+          )
+          ->add(
+            'save',
+            SubmitType::class,
+            array('label' => 'action.save')
+          );
     }
 
     /**
@@ -38,9 +59,9 @@ class AnecdoteType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(
-            array(
-                'data_class' => 'AppBundle\Entity\Anecdote',
-            )
+          array(
+            'data_class' => 'AppBundle\Entity\Anecdote',
+          )
         );
     }
 
